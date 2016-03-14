b81508494c950682a2e58682e710f4c306c8fc15
Jonathan Casey <jon_casey@me.com>
2016-03-14 15:39:50 -0600

    add sockets

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
-var routes = require('./routes/index');
+var routes = require('./routes/api');
 var users = require('./routes/users');
 var auth = require('./routes/auth');
 
-var app = express();
+server.listen(8000)//, "127.0.0.1");
+
+io.on('connection', function(socket){
+  console.log('user connected');
+  socket.emit('hello', {message: 'hello world'});
+})
+
 
 
 // uncomment after placing your favicon in /public
@@ -17,6 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api', routes);
 app.use('/users', users);
@@ -36,10 +47,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    res.json({error: err});
   });
 }
 
@@ -47,11 +55,8 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.json({error: err})
 });
 
 
-module.exports = app;
+// module.exports = app;
